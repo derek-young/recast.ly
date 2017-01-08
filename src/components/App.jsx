6d4800dc45003bf9b8298f6bf4eeb51b635be14b
Derek Young <dthomasy@gmail.com>
2017-01-08 13:30:40 -0800

    Add search functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 class App extends React.Component {
   constructor(props) {
-    //pass props into super so an instance of App will have all properties found on props
     super(props);
 
     this.state = {
+      videoList: [],
       currentVideo: window.exampleVideoData[0]
     };
+  }
+
+  componentDidMount() {
+    window.searchYouTube({query: 'skiing'}, this.updateVideo.bind(this));
   }
 
   render() {
@@ -16,7 +20,7 @@ class App extends React.Component {
           <VideoPlayer video={this.state.currentVideo}/>
         </div>
         <div className="col-md-5">
-          <VideoList videos={window.exampleVideoData} videoListClick={this.videoListClick.bind(this)}/>
+          <VideoList videos={this.state.videoList} videoListClick={this.videoListClick.bind(this)}/>
         </div>
       </div>
     );
@@ -27,10 +31,15 @@ class App extends React.Component {
       currentVideo: video
     });
   }
+
+  updateVideo(videos) {
+    this.setState({
+        videoList: videos,
+        currentVideo: videos[0]
+      });
+  }
 }
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-
